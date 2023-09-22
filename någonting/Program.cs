0b7421104066030342d125d211b37b28e3d2c7cb
Pingvu <nhlpool27@gmail.com>
2023-09-22 15:46:02 +0200

    Add project files.

---
+++ någonting/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace någonting
+{
+
+    class Program
+    {
+
+        static void Main(string[]args)
+        {
+
+            Console.WriteLine("Vilken stad är störst i Sverige?");
+            while (true)
+            {
+                string gissning = Console.ReadLine();
+
+                if (gissning == "Stockholm" || gissning == "stockholm")
+                {
+                    Console.WriteLine("Korrekt!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Försök igen.");
+
+                }
+            }
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("Vilken stad är störst i Norge?");
+            while (true)
+            {
+                string gissning = Console.ReadLine();
+
+                if (gissning == "Oslo" || gissning== "oslo")
+                {
+                    Console.WriteLine("Korrekt!");
+
+                    break;
+                    
+
+                    
+                }
+                else
+                {
+                 Console.WriteLine("Försök igen!");
+
+                   
+                    
+                 
+
+                }
+
+                
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
